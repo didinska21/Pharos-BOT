b6a64683ae0f809129efe02e0cbcad960acd8b3b
didinska21 <mr.didinska21@gmail.com>
2025-06-26 22:55:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,19 +39,6 @@ class PharosTestnet:
             {"type":"function","name":"deposit","stateMutability":"payable","inputs":[],"outputs":[]},
             {"type":"function","name":"withdraw","stateMutability":"nonpayable","inputs":[{"name":"wad","type":"uint256"}],"outputs":[]}
         ]''')
-        self.MINT_CONTRACT_ABI = [
-            {
-                "inputs": [
-                    { "internalType": "address", "name": "_asset", "type": "address" },
-                    { "internalType": "address", "name": "_account", "type": "address" },
-                    { "internalType": "uint256", "name": "_amount", "type": "uint256" }
-                ],
-                "name": "mint",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            }
-        ]
         self.SWAP_CONTRACT_ABI = [
             {
                 "inputs": [
@@ -131,7 +118,7 @@ class PharosTestnet:
         {Fore.GREEN + Style.BRIGHT}Pharos Testnet{Fore.BLUE + Style.BRIGHT} BOT
             """
             f"""
-        {Fore.GREEN + Style.BRIGHT}PHAROS BOT {Fore.YELLOW + Style.BRIGHT}D.Y.O.R
+        {Fore.GREEN + Style.BRIGHT}Rey? {Fore.YELLOW + Style.BRIGHT}<INI WATERMARK>
             """
         )
 
@@ -1033,7 +1020,7 @@ class PharosTestnet:
         return option, choose, rotate
     
     async def user_login(self, address: str, proxy=None, retries=5):
-        url = f"{self.BASE_API}/user/login?address={address}&signature={self.signatures[address]}&invite_code={self.ref_code}"
+        url = f"{self.BASE_API}/user/login?address={address}&signature={self.signatures[address]}&wallet=OKX+Wallet&invite_code={self.ref_code}"
         headers = {
             **self.headers,
             "Authorization": "Bearer null",
@@ -1768,4 +1755,4 @@ if __name__ == "__main__":
             f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
             f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
             f"{Fore.RED + Style.BRIGHT}[ EXIT ] Pharos Testnet - BOT{Style.RESET_ALL}                                       "                              
-      )
+            )
